ac682828b7808bad5ec290df793e490f82a1b136
Zachary Clark <zacclark369@gmail.com>
2022-02-20 17:06:57 -0800

    added comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,47 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData = res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment.text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData = res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData) {
+            res.status(404).json({ message: 'Comment not found' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
