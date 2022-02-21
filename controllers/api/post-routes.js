1044713ebdfa531beae1af908ea0c18432fdf0a8
Zachary Clark <zacclark369@gmail.com>
2022-02-20 23:21:05 -0800

    working on refactoring and working through post-routes. Will submit what I have and come back to it to work on i

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,78 +3,56 @@ const sequelize = require('../../config/connection');
 const { Post, User, Comment } = require('../../models');
 
 // get all users
-router.get('/', (req, res) => {
-    console.log('======================');
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      order: [['created_at', 'DESC']],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
+router.get('/', async (req, res) => {
+  try {
+      const posts = await Post.findAll(
+          {
+              attributes: ['id', 'title', 'content'],
+              order: [['created_at', 'DESC']]
           }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+      )
+
+      res.status(200).json(posts);
+  } catch (err) {
+      // return server error message
+      console.log(err);
+      res.status(500).json(err);
+  }
+})
+
   
-  router.get('/:id', (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
+router.get('/:id', async (req, res) => {
+  try {
+      const posts = await Post.findByPk(req.params.id,
+          {
+              attributes: ['id', 'title', 'content'],
+              order: [['created_at', 'DESC']],
+              include: [
+                  {
+                      model: User,
+                      attributes: ['username']
+                  },
+                  {
+                      model: Comment,
+                      attributes: ['text'],
+                      include: [
+                          {
+                              model: User,
+                              attributes: ['username']
+                          }
+                      ]
+                  }
+              ]
           }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+      )
+
+      res.status(200).json(posts);
+  } catch (err) {
+      // return server error message
+      console.log(err);
+      res.status(500).json(err);
+  }
+})
   
   router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
