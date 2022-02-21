74419721660dcb7b2c4f6a60dca5f34252cf4deb
Zachary Clark <zacclark369@gmail.com>
2022-02-20 19:36:09 -0800

    finished home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -55,6 +55,26 @@ router.get('/posts/:id', async (req, res) => {
 
         post = post.get({ plain: true});
         res.render('single-post', {post, loggedIn: req.session.loggedIn, singlePost: false});
+} catch (err) {
+    console.log(err);
+    res.status(500).json(err);
 }
-)
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) { res.redirect('/'); 
+    return;        
+}
+res.render('login');
+})
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) { res.redirect('/');
+    return;
+}
+
+res.render('signup')
+})
+
+module.exports = router;
 
